7b1f9f1b5ba8978aa71d848677bdbc30c0408434
sthzg <sthzg@gmx.net>
2014-03-22 21:34:38 +0100

    Mostly updates to the SetAdmin. Change list and change page have direct links to create/update given sets now. In conjunction with the formerly added action that can be triggered for multiple selected sets on the change list users have seamless access to this functionality.

--- filersets/admin.py
+++ filersets/admin.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 from django.contrib import admin
-from django.contrib.admin.options import ModelAdmin
+from django.core.urlresolvers import reverse
 from django.utils.translation import ugettext_lazy as _
 from suit.admin import SortableModelAdmin, SortableTabularInline
 from mptt.admin import MPTTModelAdmin
@@ -9,10 +9,10 @@ from filersets.models import Set, Item
 
 # TODO  Try to make any use of third party packages like suit and cms optional
 
-
 class ItemInlineAdmin(SortableTabularInline):
     """
     Allows to view filer_files referenced in a set to be sorted in an inline
+    form inside of the SetAdmin.
     """
     fields = ('filer_file', 'is_cover')
     list_display = ('is_cover',)
@@ -33,27 +33,53 @@ class ItemAdmin(MPTTModelAdmin, SortableModelAdmin):
     sortable = 'order'
 
 
-class SetAdmin(ModelAdmin):
+class SetAdmin(admin.ModelAdmin):
     """
     Administer a single set and show the referenced filer_files in an inline
     """
 
+    class Media:
+        js = ("filersets/filersets.js",)
+
     def create_or_update_filerset(self, request, queryset):
         """
-        This is an action that triggers the creation or update process for the
-        selected filersets. It displays in the admin change list.
-
-        TODO    Make the update routine available in different ways. e.g.
-                calling the command, using a celery if enabled, etc.
+        This action triggers the creation or update process for selected sets
+        It displays in the admin change list.
         """
         for fset in queryset:
             Set.objects.create_or_update_set(fset.id)
 
     create_or_update_filerset.short_description = _('Create/Update filerset')
 
-    list_display = ('title', 'date', 'is_processed')
+    def process_set(self, obj):
+        """
+        Extra field for change list displays a link to create / update a set
+        """
+        set_url = reverse('filersets_api:set_process_view',
+                          kwargs={'set_id': obj.pk})
+        query = '?redirect={}'.format(self.current_url)
+        label = _('Create / Update Set')
+        wrap = _('<a href="{0}{1}"><span class="icon-refresh"></span> {2}</a>')
+        return wrap.format(set_url, query, label)
+
+    process_set.allow_tags = True
+    process_set.short_description = _('Create / Update Set')
+
+    def changelist_view(self, request, extra_context=None):
+        # We need to override the changelist_view to have the current_url
+        # parameter available in the process_set function
+        self.current_url = request.get_full_path()
+        return super(SetAdmin, self).changelist_view(
+            request, extra_context=extra_context
+        )
+
+    search_fields = ('title',)
+    list_filter = ('date', 'is_processed',)
+    ordering = ('-date',)
+    list_display = ('title', 'date', 'is_processed', 'process_set',)
     inlines = (ItemInlineAdmin,)
     actions = [create_or_update_filerset]
+    readonly_fields = ('is_processed',)
 
 
 admin.site.register(Set, SetAdmin)
