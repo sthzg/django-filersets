695852eda7d23b4e7bf8dd82dba0fab9bbbd0510
sthzg <sthzg@gmx.net>
2014-10-21 20:37:29 +0200

    Constants -- Changed jquery-ui link to version that uses customized namespace.

--- filersets/admin/constants.py
+++ filersets/admin/constants.py
@@ -3,8 +3,8 @@ from __future__ import absolute_import, unicode_literals
 from django.contrib.staticfiles.templatetags.staticfiles import static
 
 
-JS_JQUERY_UI = 'filersets/vendor/jquery-ui-1.10.4/js/jquery-ui-1.10.4.min.js'
-JS_FILERSETS_ADMIN = 'filersets/js/filersets_admin.js'
+JS_JQUERY_UI = static('filersets/vendor/jquery-ui-1.10.4/js/jquery-ui-1.10.4.js')
+JS_FILERSETS_ADMIN = static('filersets/js/filersets_admin.js')
 JS_JQ_AUTOSIZE = static('filersets/vendor/jquery-autosize/jquery.autosize.min.js')
-CSS_FILERSETS_ADMIN = 'filersets/css/filersets_admin.css'
+CSS_FILERSETS_ADMIN = static('filersets/css/filersets_admin.css')
 CSS_FONTAWESOME = '//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css'
